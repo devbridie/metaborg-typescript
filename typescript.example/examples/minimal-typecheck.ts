5612c09fb9cac825fbbeb7dea7f93dcab15cfa79
Thomas Smith <t.n.smith@student.tudelft.nl>
2017-06-19 15:08:17 +0200

    Fix methods

--- typescript.example/examples/minimal-typecheck.ts
+++ typescript.example/examples/minimal-typecheck.ts
@@ -1,18 +1,25 @@
 class Vehicle {
 	wheels: number
+	print(): string {
+	 
+	}
 }
 
 class Car extends Vehicle {
 	engine: string
+	bliep(): number {}
 } 
  
 var c: Car = new Car();
 const x = c.wheels;
 
+const s: string = c.print();
+
 interface E {
 	engine: string,
-	wheels: number
+	bliep: any
 }
-//var z: E = c;
+
+var z: E = c;
 
 //var x: Car = z;
