f887dba92783e8675829fee340780969c8c93e96
Thomas Smith <tnsmith@live.nl>
2017-06-20 14:20:49 +0200

    Differentiate between value namespace lookup and type namespace lookup

--- typescript.example/examples/minimal-typecheck.js
+++ typescript.example/examples/minimal-typecheck.js
@@ -1,14 +1,33 @@
-var Car = (function () {
-    function Car(engine) {
-        this.engine = engine;
-    }
-    Car.prototype.disp = function () {
-        console.log("Engine is  :   " + this.engine);
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = Object.setPrototypeOf ||
+        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
     };
-    return Car;
+})();
+var Vehicle = (function () {
+    function Vehicle() {
+    }
+    Vehicle.prototype.print = function () {
+        return "I have wheels";
+    };
+    return Vehicle;
 }());
-;
-;
-var c = new Car("");
-var d = c;
-c = d;
+var Car = (function (_super) {
+    __extends(Car, _super);
+    function Car() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    Car.prototype.bliep = function () { return 42; };
+    return Car;
+}(Vehicle));
+var a = number;
+var c = new a();
+var b = c.wheels;
+var s = c.print();
+var q = Car;
+var z = c;
+var x = z;
--- typescript.example/examples/minimal-typecheck.ts
+++ typescript.example/examples/minimal-typecheck.ts
@@ -1,25 +1,30 @@
 class Vehicle {
 	wheels: number
 	print(): string {
-	 
+	  return "I have wheels";
 	}
 }
 
+
 class Car extends Vehicle {
 	engine: string
-	bliep(): number {}
-} 
- 
-var c: Car = new Car();
-const x = c.wheels;
+	bliep(): number { return 42; }
+}
 
-const s: string = c.print();
+//const some: Car = undefined;
+
+var a = Car;
+var c: Car = new a();
+const b = c.wheels;
+
+const s: string = c.print(); 
 
 interface E {
 	engine: string,
 	bliep: any
 }
 
+var q = Car;
 var z: E = c;
 
-//var x: Car = z;
+var x: Car = z;
