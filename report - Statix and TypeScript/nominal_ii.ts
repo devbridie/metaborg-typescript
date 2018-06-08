8a1bca83dff33d471c809d2a0a8a10735b766709
Nouri Khalass <n.khalass@gmail.com>
2018-06-08 13:04:42 +0200

    Expand section on nominal typing
    Co-authored-by: Michiel van Spaendonck <mvanspaendonck@users.noreply.github.com>

--- report - Statix and TypeScript/nominal_ii.ts
+++ report - Statix and TypeScript/nominal_ii.ts
@@ -6,12 +6,13 @@ interface Bar {
 interface Foo {
     x: string;
     y: number;
+    z: boolean;
 }
 
 function doSomething(val: Bar) {
     
 }
 
-let v: Bar = { x: "LOL", y: 42 };
-let w: Foo = v;
+let v: Bar = { x: "", y: 42, z: false }; // Error
+let w: Foo = v; // Error
 doSomething(w);
