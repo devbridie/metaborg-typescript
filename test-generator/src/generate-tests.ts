7324d600d976b2973c29377462a86b0f2810a8b4
Dereck Bridie <d.j.bridie@student.tudelft.nl>
2019-05-01 19:54:33 +0200

    Change to parse tests; add banned words

--- test-generator/src/generate-tests.ts
+++ test-generator/src/generate-tests.ts
@@ -3,6 +3,8 @@ import * as fs from 'fs';
 import * as path from 'path';
 import * as rimraf from 'rimraf'
 
+const bannedWords = ["async", "await", "function*", "@", "enum", "module", "namespace", "yield", "declare"];
+
 const from = "../typescript-reference/tests";
 const to = "../typescript.test/generated";
 (async () => {
@@ -11,14 +13,17 @@ const to = "../typescript.test/generated";
     const files = await promisify(fs.readdir)(`${from}/cases/compiler`);
     for (const file of files) {
         const fromFile = `${from}/cases/compiler/${file}`;
-        const noExtension = path.basename(fromFile.slice(0, fromFile.length - 3));
+        const contents = await promisify(fs.readFile)(`${fromFile}`, {encoding: 'utf8'});
+        if (bannedWords.some(ban => contents.includes(ban))) continue;
+
+        const noExtension = path.basename(fromFile.slice(0, fromFile.length - 3)).replace(/\W/g,'_');
         const fromErrorFile = `${from}/baselines/reference/${noExtension}.errors.txt`;
         let errorContents = null;
         try {
             errorContents = await promisify(fs.readFile)(fromErrorFile, {encoding: 'utf8'});
         } catch (e) {
         }
-        const contents = await promisify(fs.readFile)(`${fromFile}`, {encoding: 'utf8'});
+
         const toFile = `${to}/${file}.spt`;
         if (errorContents) {
             await promisify(fs.writeFile)(toFile, `module generated_${noExtension}
@@ -37,11 +42,13 @@ language typescript
 start symbol Program
 test generated [[[[
 ${contents}
-]]]] 0 errors  
+]]]] parse succeeds  
         `, {encoding: 'utf8'})
 
         }
         console.log("Wrote", fromFile, "to", toFile);
         // break;
     }
+    const total = (await promisify(fs.readdir)(to)).length;
+    console.log(`Wrote ${total} spt files.`)
 })();
