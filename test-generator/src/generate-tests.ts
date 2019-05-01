8f50b1c29c41909c03f03f5e8cc1e1ab364fbffb
Dereck Bridie <d.j.bridie@student.tudelft.nl>
2019-05-01 13:19:41 +0200

    Add generation fix on older nodes

--- test-generator/src/generate-tests.ts
+++ test-generator/src/generate-tests.ts
@@ -1,23 +1,27 @@
-import {promises} from 'fs';
+import {promisify} from 'util';
+import * as fs from 'fs';
 import * as path from 'path';
+import * as rimraf from 'rimraf'
 
 const from = "../typescript-reference/tests";
 const to = "../typescript.test/generated";
 (async () => {
-    const files = await promises.readdir(`${from}/cases/compiler`);
+    await rimraf.sync(to);
+    await promisify(fs.mkdir)(to, { recursive: true});
+    const files = await promisify(fs.readdir)(`${from}/cases/compiler`);
     for (const file of files) {
         const fromFile = `${from}/cases/compiler/${file}`;
         const noExtension = path.basename(fromFile.slice(0, fromFile.length - 3));
         const fromErrorFile = `${from}/baselines/reference/${noExtension}.errors.txt`;
         let errorContents = null;
         try {
-            errorContents = await promises.readFile(fromErrorFile, {encoding: 'utf8'});
+            errorContents = await promisify(fs.readFile)(fromErrorFile, {encoding: 'utf8'});
         } catch (e) {
         }
-        const contents = await promises.readFile(`${fromFile}`, {encoding: 'utf8'});
+        const contents = await promisify(fs.readFile)(`${fromFile}`, {encoding: 'utf8'});
         const toFile = `${to}/${file}.spt`;
         if (errorContents) {
-            await promises.writeFile(toFile, `module generated_${noExtension}
+            await promisify(fs.writeFile)(toFile, `module generated_${noExtension}
 
 language typescript
 start symbol Program
@@ -27,7 +31,7 @@ ${contents}
         `, {encoding: 'utf8'})
 
         } else {
-            await promises.writeFile(toFile, `module generated_${noExtension}
+            await promisify(fs.writeFile)(toFile, `module generated_${noExtension}
 
 language typescript
 start symbol Program
