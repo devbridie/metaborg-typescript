0534831b56b2e04b24e5b7a896bb0ab167ceece9
Dereck Bridie <d.j.bridie@student.tudelft.nl>
2019-05-13 18:41:35 +0200

    Add tsc reference strategy

--- typescript/src/main/strategies/typescript/strategies/reference_compiler_0_0.java
+++ typescript/src/main/strategies/typescript/strategies/reference_compiler_0_0.java
@@ -1,13 +1,35 @@
 package typescript.strategies;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.nio.charset.Charset;
+
+import org.apache.commons.io.IOUtils;
 import org.spoofax.interpreter.terms.IStrategoTerm;
 import org.spoofax.terms.StrategoString;
+import org.spoofax.terms.TermFactory;
 import org.strategoxt.lang.Context;
 import org.strategoxt.lang.Strategy;
+
+import com.google.common.io.Files;
 
 public class reference_compiler_0_0 extends Strategy {
 	public static reference_compiler_0_0 instance = new reference_compiler_0_0();
 	public IStrategoTerm invoke(Context context, IStrategoTerm current) {
-		return null;
+		try {
+			String inputProgram = ((StrategoString) current).stringValue();
+			File tmpFile = File.createTempFile("tsc", ".ts");
+			try (BufferedWriter writer = Files.newWriter(tmpFile, Charset.defaultCharset())) {
+				writer.write(inputProgram);
+				writer.flush();
+			}
+			Process tsc = new ProcessBuilder("tsc", "--noEmit", "--target", "ESNext", tmpFile.getAbsolutePath()).start();
+			int out = tsc.waitFor();
+			if (out == 0) return new StrategoString("success", TermFactory.EMPTY_LIST, IStrategoTerm.IMMUTABLE);
+			else return new StrategoString("fail\n" + IOUtils.toString(tsc.getInputStream(), Charset.forName("utf8")), TermFactory.EMPTY_LIST, IStrategoTerm.IMMUTABLE);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
 	}
 }
